373c73c3d0d1298defcc8272b21a5a0024d796ae
Dany Rose <danyrose1995@gmail.com>
2024-12-11 13:43:39 +0100

    fix: remove "score" from "Strategy" class

--- Strategy.php
+++ Strategy.php
@@ -2,7 +2,6 @@
 
 abstract class Strategy
 {
-    protected int $score = 0;
     abstract protected function getScore(): int;
     abstract protected function updateScore(int $value): int;
     abstract protected function play(array $history): bool;
