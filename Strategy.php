9aa15608f16a0195a0ff612059fbf31721df0a7f
Dany Rose <danyrose1995@gmail.com>
2024-12-11 13:18:56 +0100

    fix: changed return type of "getScore" function from "string" to "int" in "Strategy" class

--- Strategy.php
+++ Strategy.php
@@ -3,7 +3,7 @@
 abstract class Strategy
 {
     protected int $score = 0;
-    abstract protected function getScore(): string;
+    abstract protected function getScore(): int;
     abstract protected function updateScore(int $value): int;
     abstract protected function play(array $history): bool;
 }
