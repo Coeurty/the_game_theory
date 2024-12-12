133bab8aadc4616f8388591758de28e94fd6e901
Clément <clementloire2@gmail.com>
2024-12-12 15:37:22 +0100

    feat: added "Simpleton" strategy

---
+++ strategies/Simpleton.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+require_once "Strategy.php";
+class Simpleton extends Strategy
+{
+
+    private int $score = 0;
+    public function getScore(): int
+    {
+        return $this->score;
+    }
+    public function updateScore(int $value): int
+    {
+        $this->score += $value;
+        return $this->score;
+    }
+    public function play(array $history, string $selfLabel, string $opponentLabel): bool
+    {
+        if (empty($history)) {
+            return true;
+        }
+
+        $lastMove = end($history);
+        $selfLastMove = $lastMove[$selfLabel];
+        $opponentLastMove = $lastMove[$opponentLabel];
+
+        if ($opponentLastMove === true) {
+            return $selfLastMove;
+        }
+
+        if ($opponentLastMove === false) {
+            return !$selfLastMove;
+        }
+
+        return true;
+    }
+}
