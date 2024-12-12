46df9576b9f1bb0a5b7aeb4de093433dd1868205
Dany Rose <danyrose1995@gmail.com>
2024-12-12 15:40:24 +0100

    fix: added name to "Simpleton" class

--- strategies/Simpleton.php
+++ strategies/Simpleton.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 require_once "Strategy.php";
 class Simpleton extends Strategy
 {
-
+    private string $name = "Simpleton";
     private int $score = 0;
+    public function getName(): string
+    {
+        return $this->name;
+    }
     public function getScore(): int
     {
         return $this->score;
