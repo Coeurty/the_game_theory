f28697cef5fa84cbcea8e435616474df4e9a083c
Dany Rose <danyrose1995@gmail.com>
2024-12-12 09:54:35 +0100

    feat: added "Grudger" strategy

---
+++ strategies/Grudger.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "./Strategy.php";
+
+class Grudger extends Strategy
+{
+    private int $score = 0;
+    public function getScore(): int
+    {
+        return $this->score;
+    }
+    public function updateScore(int $value): int
+    {
+        $this->score += $value;
+        return $this->score;
+    }
+    public function play(array $history, string $selfLabel, string $opponentLabel): bool
+    {
+        $willCooperate = true;
+        $opponentPlayHistory = array_map(fn($round) => $round[$opponentLabel], $history);
+        $opponentHasCheated = in_array(false, $opponentPlayHistory);
+        $willCooperate = $opponentHasCheated ? false : true;
+        return $willCooperate;
+    }
+}
